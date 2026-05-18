32f2ef985f12766507d395dc821efb9ecafdc333
Christopher Johnstone <chrisj97@proton.me>
2026-05-18 21:14:15 +0100

    Updated formatting options

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 #define ENVAR_COMMAND "BROWSER"
 #define ENVAR_BASE_URL "JIRA_BASE_URL"
+#define ENVAR_DEFAULT_PROJECT "JIRA_DEFAULT_PROJECT"
 
 void print_usage() {
     printf("Usage: program [arguments]\n");
@@ -33,10 +34,19 @@ char *get_base_url() {
 
 char *get_default_project() {
     // TODO: Support config
+    const char *command = getenv(ENVAR_DEFAULT_PROJECT);
+    if (command) {
+        return strdup(command);
+    }
     return NULL;
 }
 
-void open_ticket(char *arg) {
+void open_ticket(
+    char *arg,
+    char *command,
+    char *base_url,
+    char *default_project
+) {
     printf("Opening ticket for: %s\n", arg);
     // Simulate ticket opening logic here
 }
@@ -45,24 +55,19 @@ int main(int argc, char *argv[]) {
     char *command = get_command();
     char *base_url = get_base_url();
     char *default_project = get_default_project();
-    printf("Command: %s\nBase URL: %s\nDefault Project: %s\n",
-           command,
-           base_url,
-           default_project);
-
     bool has_input = false;
     char line[256];
     if (argc > 1) {
         has_input = true;
         for (int i = 1; i < argc; i++) {
-            open_ticket(argv[i]);
+            open_ticket(argv[i], command, base_url, default_project);
         }
     }
     if (!isatty(0)) {
         has_input = true;
         while (fgets(line, sizeof(line), stdin)) {
             line[strcspn(line, "\n")] = '\0';
-            open_ticket(line);
+            open_ticket(line, command, base_url, default_project);
         }
     }
     if (!has_input) {
